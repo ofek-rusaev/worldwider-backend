822860725eec926bd051124215e11310b523331b
kerensoko86 <keren.sokoletsky@gmail.com>
2020-03-24 20:18:41 +0200

    loadreviews

--- api/review/review.service.js
+++ api/review/review.service.js
@@ -3,50 +3,50 @@ const dbService = require('../../services/db.service')
 const ObjectId = require('mongodb').ObjectId
 
 async function query(filterBy = {}) {
-    // const criteria = _buildCriteria(filterBy)
+    const criteria = _buildCriteria(filterBy)
     const collection = await dbService.getCollection('review')
     try {
-        // const reviews = await collection.find(criteria).toArray();
-        var reviews = await collection.aggregate([
-            {
-                $match: filterBy
-            },
-            {
-                $lookup:
-                {
-                    from: 'user',
-                    localField: 'byUserId',
-                    foreignField: '_id',
-                    as: 'byUser'
-                }
-            }, 
-            {
-                $unwind: '$byUser'
-            },
-            {
-                $lookup:
-                {
-                    from: 'user',
-                    localField: 'aboutUserId',
-                    foreignField: '_id',
-                    as: 'aboutUser'
-                }
-            }, 
-            {
-                $unwind: '$aboutUser'
-            }
-        ]).toArray()
+        const reviews = await collection.find(criteria).toArray();
+        // var reviews = await collection.aggregate([
+        //     {
+        //         $match: filterBy
+        //     },
+        //     {
+        //         $lookup:
+        //         {
+        //             from: 'user',
+        //             localField: 'byUserId',
+        //             foreignField: '_id',
+        //             as: 'byUser'
+        //         }
+        //     }, 
+        //     {
+        //         $unwind: '$byUser'
+        //     },
+        //     {
+        //         $lookup:
+        //         {
+        //             from: 'user',
+        //             localField: 'aboutUserId',
+        //             foreignField: '_id',
+        //             as: 'aboutUser'
+        //         }
+        //     }, 
+        //     {
+        //         $unwind: '$aboutUser'
+        //     }
+        // ])
+        // .toArray()
 
-        reviews = reviews.map(review => {
-            review.byUser = {_id: review.byUser._id, username: review.byUser.username}
-            review.aboutUser = {_id: review.aboutUser._id, username: review.aboutUser.username}
-            delete review.byUserId;
-            delete review.aboutUserId;
-            return review;
-        })
-
-        return reviews
-    } catch (err) {
+        // reviews = reviews.map(review => {
+        //     review.byUser = {_id: review.byUser._id, username: review.byUser.username}
+        //     review.aboutUser = {_id: review.aboutUser._id, username: review.aboutUser.username}
+        //     delete review.byUserId;
+        //     delete review.aboutUserId;
+        return reviews;
+    }
+    // return reviews    }
+    catch (err) {
         console.log('ERROR: cannot find reviews')
         throw err;
     }
@@ -55,7 +55,7 @@ async function query(filterBy = {}) {
 async function remove(reviewId) {
     const collection = await dbService.getCollection('review')
     try {
-        await collection.deleteOne({"_id":ObjectId(reviewId)})
+        await collection.deleteOne({ "_id": ObjectId(reviewId) })
     } catch (err) {
         console.log(`ERROR: cannot remove review ${reviewId}`)
         throw err;
