543786e382b9f379d1ea1983b910e44127afda91
ofek-rusaev <ofek2602@gmail.com>
2020-03-25 22:15:15 +0200

    adding reviews

--- api/review/review.service.js
+++ api/review/review.service.js
@@ -5,6 +5,7 @@ const COLLECTION_NAME = "review";
 
 async function query(filterBy = {}) {
     const criteria = _buildCriteria(filterBy);
+
     const collection = await dbService.getCollection(COLLECTION_NAME);
     try {
         var reviews = await collection
@@ -49,7 +50,6 @@ async function query(filterBy = {}) {
 }
 
 async function remove(reviewId) {
-    console.log('keren', reviewId)
     const collection = await dbService.getCollection(COLLECTION_NAME);
     try {
         await collection.deleteOne({ _id: ObjectId(reviewId) });
@@ -60,11 +60,10 @@ async function remove(reviewId) {
 }
 
 async function add(review) {
-    review.byUserId = ObjectId(review.byUserId);
-    review.aboutUserId = ObjectId(review.aboutUserId);
+    review.userId = ObjectId(review.userId);
+    review.tourGuideId = ObjectId(review.tourGuideId);
     review.createdAt = Date.now();
     const collection = await dbService.getCollection(COLLECTION_NAME);
-    console.log('new review', review)
     try {
         await collection.insertOne(review);
         return review;
